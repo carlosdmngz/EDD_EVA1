8408a66e5edc7220d058e4557157d1aadcdcdacd
carlosdmngz <54371805+carlosdmngz@users.noreply.github.com>
2019-10-05 22:54:13 -0600

    Add files via upload

---
+++ EVA1_2_MEMORY_MANAGEMENT/src/Principal.java
@@ -0,0 +1,10 @@
+public class Principal {
+    public static void main(String[] args) {
+        int u = 10;
+        Principal pObj = new Principal();
+        System.out.println(u);
+        System.out.println(pObj);
+        pObj = null; //NOS DESHACEMOS DE LA MEMORIA USADA
+    }  
+}
+
